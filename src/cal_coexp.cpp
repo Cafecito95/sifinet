876eb280feff337743e1d00a193e03ccebd4d31e
Cafecito95 <71472156+Cafecito95@users.noreply.github.com>
2024-05-06 23:49:10 -0400

    Update cal_coexp.cpp

--- src/cal_coexp.cpp
+++ src/cal_coexp.cpp
@@ -14,10 +14,10 @@ arma::mat cal_coexp(arma::mat X, arma::mat X_subcohort){
   int n = X.n_rows;
   arma::vec q(p);
   for(int i = 0; i < p; i++){
-    q(i) = mean(X.col(i));
+    q(i) = mean(X_subcohort.col(i));
   }
   arma::vec mq = 1 - q;
-  arma::mat c = X_subcohort.t() * X_subcohort - q * q.t() * n;
+  arma::mat c = X_subcohort.t() * X_subcohort - q * q.t() * X_subcohort.n_rows;
   arma::mat d = sqrt(X_subcohort.n_rows * q * q.t() % (mq * mq.t()));
   
   return(c / d);
